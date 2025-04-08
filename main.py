f9e841c30eb66775180ee75743a6bf3f4c72bb58
Rachel Jaisey <jaiseyrachel@gmail.com>
2025-04-08 13:51:23 +0100

    feat: add function to check for Pythagorean Triple

---
+++ main.py
@@ -0,0 +1,8 @@
+
+def is_pythagorean_triple(a, b, h):
+    if a**2 * b**2 == h**2:
+        print("This triangle is a Pythagoeran Triple")
+    else:
+        print("This triangle is NOT a Pythagorean Triple")
+
+
