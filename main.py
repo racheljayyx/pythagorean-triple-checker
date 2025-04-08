02575c6869b40787d577cb5432dd3b8d42d3a8ec
Rachel Jaisey <jaiseyrachel@gmail.com>
2025-04-08 14:34:12 +0100

    feat: add get_positive_integer function to validate user input is a postive integer

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-
 def is_pythagorean_triple(a, b, h):
-    if a**2 * b**2 == h**2:
+    if a ** 2 * b ** 2 == h ** 2:
         print("This triangle is a Pythagorean Triple")
     else:
         print("This triangle is NOT a Pythagorean Triple")
+
+
+def get_positive_integer(prompt):
+    while True:
+        try:
+            num = int(input(prompt))
+            if num <= 0:
+                raise ValueError
+            break
+        except ValueError:
+            print("Invalid input.")
+            prompt = "Enter a positive integer: "
 
