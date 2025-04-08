a1cdacb2d35c8640b83d10ecfac42afa997574b2
Rachel Jaisey <jaiseyrachel@gmail.com>
2025-04-08 14:58:07 +0100

    fix(is_pythagorean_triple): correct pythagoras theorem equation and remove line to check list has been sorted in main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def is_pythagorean_triple(a, b, h):
-    if a ** 2 * b ** 2 == h ** 2:
+    if a ** 2 + b ** 2 == h ** 2:
         print("This triangle is a Pythagorean Triple")
     else:
         print("This triangle is NOT a Pythagorean Triple")
